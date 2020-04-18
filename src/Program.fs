6469e23acf4dc97c71a4d9e4716519dec93878be
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-04-19 01:22:18 +0900

    Do more logging on src/Program.fs

--- src/Program.fs
+++ src/Program.fs
@@ -28,20 +28,28 @@ let main argv =
         | TypingError(e, t1, t2) -> reraise()
         | UndefinedVariableError(x, loc) -> reraise()
 
+    eprintfn "typed:\n%A" typed
+
     let knorm =
         try
             KNormal.run' typed
         with KNormalizationError(_) -> reraise()
+    eprintfn "knorm:\n%A" knorm
 
     let alpha = Alpha.run' knorm
     let beta = Beta.run' alpha
     let assoc = Assoc.run beta
     let (closure, toplevel) = Closure.run assoc
-    let (main, toplevel) = Virtual.run closure toplevel
-    let asm = RegAlloc.run main toplevel
-    let code = Emit.run asm
+    eprintfn "closure:\n(%A, %A)" closure toplevel
 
+    let (main, toplevel) = Virtual.run closure toplevel
+    eprintfn "virtual:\n(%A, %A)" main toplevel
+
+    let asm = RegAlloc.run main toplevel
+    eprintfn "asm:"
     for op in asm do
         eprintfn "%A" op
+
+    let code = Emit.run asm
     printf "%s" code
     0
